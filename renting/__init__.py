2e129f3f78f16e9251fe983f90acdc3a1560a413
salimmapande <salimmapande@gmail.com>
2022-01-04 00:53:42 +0300

    chanhes

--- renting/__init__.py
+++ renting/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 UPLOAD_FOLDER = 'renting/static/uploads/'
 
@@ -7,11 +9,15 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rentaldb.db'
 app.config['SECRET_KEY'] = '53a089e23b6255700b33f9fc'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-# from renting.models import HouseProperty, Tenant, TenantPayments
+# from renting.models import HouseProperty, Tenant, TenantPayments, User
 # db.drop_all()
 # db.create_all()
 # db.session.commit()
 
 from renting import routes
+#from renting import send_sms
